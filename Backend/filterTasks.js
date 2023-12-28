0a9b6b954276fa4a6d47195359c6f1d720ac7a54
Mirko <mirkosaavedra596@gmail.com>
2023-12-28 17:53:38 +0100

    fix a bug

--- Backend/filterTasks.js
+++ Backend/filterTasks.js
@@ -1,6 +1,6 @@
 // This function filterTasks takes two arguments: 
 // tasks, which is an array of task objects, and filterCriteria, 
-// which is an object containing filter criteria such as name and status.
+// which is an object containing filter criteria such as type and time.
 // The JavaScript filter function is used to filter the array of tasks based on the specified criteria.
 export function filterTask(tasks, filterCriteria) {
     return tasks.filter((task) => {
